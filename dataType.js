1c3ac9e53e589987a57bc524337e0709bf8d5cb8
Shobhana-MG <76728628+Shobhana-MG@users.noreply.github.com>
2021-02-04 16:52:01 +0530

    Add files via upload

---
+++ dataType.js
@@ -0,0 +1,50 @@
+//for reference
+// number data type
+
+var num=100;
+console.log(num)
+
+// strings
+
+var string = "shobhana";
+console.log(string)
+
+// boolean 
+var bool =true
+console.log(bool)
+
+// undefined 
+var object 
+console.log(object);
+
+// null
+object=null;
+console.log(object)
+
+
+//array holding same datatype
+
+var arr1=[1,2,3,4]
+console.log(arr1);
+
+
+// array holding diff datatype
+var arr2= ["name",12,true]
+
+console.log(arr2);
+
+
+// array storing list of arrays
+
+var arr3=[ [1,2],[3,4],[5,6, "left"]]
+console.log(arr3);
+console.log(arr3[1]);
+console.log(arr3[2][2]);
+console.log(arr3[1][1])
+
+
+arr3.push("shobhana")
+arr3.push(true);
+console.log(arr3);
+
+arr3.pop();
